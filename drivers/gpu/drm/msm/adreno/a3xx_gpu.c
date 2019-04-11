38b6e2df2478030456c4fa3dcabbd898db93f41e
hector <hecvalve@hotmail.com>
2019-04-11 23:30:02 +0200

    fix merge

--- drivers/gpu/drm/msm/adreno/a3xx_gpu.c
+++ drivers/gpu/drm/msm/adreno/a3xx_gpu.c
@@ -2,7 +2,7 @@
  * Copyright (C) 2013 Red Hat
  * Author: Rob Clark <robdclark@gmail.com>
  *
- * Copyright (c) 2014,2018 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as published by
